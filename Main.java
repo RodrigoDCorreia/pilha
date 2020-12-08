bc1f0fc2669bbe92c02d19d715f29e1c46013ba2
Rodrigo Dellagnolo Correia <66848022+RodrigoDCorreia@users.noreply.github.com>
2020-12-08 19:45:11 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Stack c = new ArrayStack();
+        System.out.println(c);
+        System.out.println("Pilha: " + c.getTam());
+
+    }
+
+}
---
+++ Stack.java
@@ -0,0 +1,7 @@
+public interface Stack<T> {
+    void push(T data);
+    T pop();
+    boolean isEmpty();
+    int getTam();
+
+}
